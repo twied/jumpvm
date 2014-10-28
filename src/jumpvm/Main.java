ff83de3a52145dd71cac8d5e329bbe50501ec025
Tim Wiederhake <Tim.Wiederhake@uni-ulm.de>
2014-10-28 14:32:57 +0100

    Add presentation slides.

--- src/jumpvm/Main.java
+++ src/jumpvm/Main.java
@@ -52,7 +52,7 @@ public final class Main {
     }
 
     /** JumpVM version. */
-    public static final String VERSION = "0.7";
+    public static final String VERSION = "1.0";
 
     /**
      * Convenience method to read an ImageIcon resource.
